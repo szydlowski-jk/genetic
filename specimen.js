403f416fd74a735b7a9c31e6cf407fa0089b05b0
Jacek Szydlowski <jacek.szydlowski@synapse.com>
2019-06-26 16:23:49 +0200

    WIP - Specimen genotype specification

--- specimen.js
+++ specimen.js
@@ -1,6 +1,17 @@
+function clone (obj) {return JSON.parse(JSON.stringify(obj))}
+
+class Genotype = {
+    constructor () {
+        this.speed = 0
+        this.sensorRange = 0
+        this.sensorRGB = {r:0, g:0, b:0}
+        this.lightRGB = {r:0, g:0, b:0}
+        this.energy = 0
+    }
+}
 
 class Specimen {
     constructor () {
-        
+        genes
     }
 }
