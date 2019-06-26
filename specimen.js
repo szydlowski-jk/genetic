3024318ba3576f3be65432e533fa417055f2ca4d
Jacek Szydlowski <szydlowski.jk@gmail.com>
2019-06-26 23:54:34 +0200

    WIP - Specimen update

--- specimen.js
+++ specimen.js
@@ -1,17 +1,17 @@
 function clone (obj) {return JSON.parse(JSON.stringify(obj))}
 
-class Genotype = {
+class Genotype {
     constructor () {
         this.speed = 0
         this.sensorRange = 0
         this.sensorRGB = {r:0, g:0, b:0}
         this.lightRGB = {r:0, g:0, b:0}
-        this.energy = 0
+        this.energyCapacity = 0
     }
 }
 
 class Specimen {
     constructor () {
-        genes
+        genes = new Genotype()
     }
 }
